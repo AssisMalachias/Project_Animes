d15fb95818de33b8d671a1bb9da838d62fe61568
Francisco Assis Malachias de Almeida <127680274+AssisMalachias@users.noreply.github.com>
2025-07-07 15:05:23 -0300

    Crie hero tape variable em crie pasta assets font esxporte ele para arquivo main.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function style() {
     return gulp.src('./src/css/*.scss')
-    .pipe(sass({ outputStyle: 'campressed' }))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./dist/css'));
 }
-
-exports.default = style;
-exports.watch = function() {
-    gulp.watch('./src/css/*.scss', gulp.parallel(style))
+function optimizeImagens() {
+    return gulp.src('./src/image/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/image'));
 }
+
+exports.default = gulp.series(
+    gulp.parallel(style, optimizeImagens),
+function watchFiles() {
+    gulp.watch('./src/css/*.scss', style);
+  }
+);
