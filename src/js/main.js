9dfb4f26c8abbf1bccd1b7f53c1f0f1bea2491b7
Francisco Assis Malachias de Almeida <127680274+AssisMalachias@users.noreply.github.com>
2025-07-18 18:24:36 -0300

    Crie o cabecalho

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
      const showsbuttons =  document.querySelectorAll('[data-tab-button]');
-     const questions = document.querySelectorAll('[data-faq-question]')
+     const questions = document.querySelectorAll('[data-faq-question]');
+
+
+     const heroSection = document.querySelector('.hero');
+     const alturaHero = heroSection.clientHeight;
+
+
+     window.addEventListener('scroll', function() {
+        const posicaoAtual =  window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+           ocultaElementoDoHeader();
+        }else {
+            exibeElementoDoHeader();
+        }
+     })
   
-    
+    // Seção de atraçôes, programação das abas 
     for(let i = 0; i < showsbuttons.length; i++){
         showsbuttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +28,22 @@ document.addEventListener('DOMContentLoaded', function() {
              botao.target.classList.add('show__tabs__button--is-active');
         })
     }
+
+    // Seção FAQ, accordion 
     for(let i = 0; i <  questions.length; i++){
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function ocultaElementoDoHeader() {
+    const header  = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementoDoHeader() {
+    const header  = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
 
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
